17636a8da95a5571a16480c980589bea95f1d77c
Özkan Uysal <61411331+ozkanuysal@users.noreply.github.com>
2025-07-22 14:23:57 +0300

    style: Consistent use of double quotes in string literals across files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ DESCRIPTION = "py-py-kick: Advanced python library for py-py-kick"
 
 
 def get_requirements(req_path: str):
-    with open(req_path, encoding='utf8') as f:
+    with open(req_path, encoding="utf8") as f:
         return f.read().splitlines()
 
 
@@ -17,28 +17,28 @@ INSTALL_REQUIRES = get_requirements("requirements.txt")
 
 def get_long_description():
     base_dir = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(base_dir, 'README.md'), encoding='utf-8') as f:
+    with open(os.path.join(base_dir, "README.md"), encoding="utf-8") as f:
         return f.read()
 
 
 def get_version():
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    version_file = os.path.join(current_dir, NAME, '__init__.py')
-    with open(version_file, encoding='utf-8') as f:
+    version_file = os.path.join(current_dir, NAME, "__init__.py")
+    with open(version_file, encoding="utf-8") as f:
         return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
 
 
 def get_author():
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    init_file = os.path.join(current_dir, NAME, '__init__.py')
-    with open(init_file, encoding='utf-8') as f:
+    init_file = os.path.join(current_dir, NAME, "__init__.py")
+    with open(init_file, encoding="utf-8") as f:
         return re.search(r'^__author__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
 
 
 def get_license():
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    init_file = os.path.join(current_dir, NAME, '__init__.py')
-    with open(init_file, encoding='utf-8') as f:
+    init_file = os.path.join(current_dir, NAME, "__init__.py")
+    with open(init_file, encoding="utf-8") as f:
         return re.search(r'^__license__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
 
 
@@ -46,12 +46,12 @@ setuptools.setup(
     name=NAME,
     version=get_version(),
     author=get_author(),
-    author_email='ozkan@uysal.dev',
+    author_email="ozkan@uysal.dev",
     license=get_license(),
     description=DESCRIPTION,
     long_description=get_long_description(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/ozkanuysal/py-py-kick',
+    long_description_content_type="text/markdown",
+    url="https://github.com/ozkanuysal/py-py-kick",
     install_requires=INSTALL_REQUIRES,
     packages=setuptools.find_packages(),
 )
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -10,6 +10,7 @@ def test_import_main_package():
 
 def test_package_has_version():
     import py_py_kick
+
     assert hasattr(py_py_kick, "__version__"), "__version__ attribute missing"
     assert hasattr(py_py_kick, "__author__"), "__author__ attribute missing"
     assert hasattr(py_py_kick, "__license__"), "__license__ attribute missing"
